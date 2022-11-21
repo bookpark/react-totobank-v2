80afe6c496819bd09fb2bdc1ee1524e0f987b8f4
bookpark <bkbkgg@gmail.com>
2022-11-21 11:10:52 +0900

    id, name, password, option을 위한 change 함수와 grade 초기화 하는 코드 추가

--- src/component/MakeAccount.js
+++ src/component/MakeAccount.js
@@ -23,8 +23,18 @@ class MakeAccount extends Component {
         }
     }
 
+    // id, name, password, option을 위한 change 함수
+    change = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        this.setState({ acc: { ...this.state.acc, [name]: value } });
+    }
+
     changeSpecial = (e) => {
         this.setState({ special: e.target.checked })
+        if (!e.target.checked) {
+            this.setState({ acc: { ...this.state.acc, grade: '' } });
+        }
     }
 
     toggle = () => {
@@ -38,7 +48,7 @@ class MakeAccount extends Component {
                     <FormGroup row>
                         <Label for="id" sm={4}>계 좌&nbsp; 번 호</Label>
                         <Col sm={5}>
-                            <Input type="text" name="id" id="id" sm={6} value={this.state.acc.id} />
+                            <Input type="text" name="id" id="id" sm={6} value={this.state.acc.id} onChange={this.change} />
                         </Col>
                         <Col>
                             <Button sm={2} color='primary' style={{ width: '100px' }}>중복</Button>
@@ -47,13 +57,13 @@ class MakeAccount extends Component {
                     <FormGroup row>
                         <Label for="name" sm={4}>이&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;름</Label>
                         <Col>
-                            <Input type="text" name="name" id="name" sm={8} value={this.state.acc.name} />
+                            <Input type="text" name="name" id="name" sm={8} value={this.state.acc.name} onChange={this.change} />
                         </Col>
                     </FormGroup>
                     <FormGroup row>
                         <Label for="password" sm={4}>비 밀&nbsp; 번 호</Label>
                         <Col>
-                            <Input type="password" name="password" id="password" sm={8} value={this.state.acc.password} />
+                            <Input type="password" name="password" id="password" sm={8} value={this.state.acc.password} onChange={this.change} />
                         </Col>
                     </FormGroup>
                     <FormGroup row>
@@ -62,7 +72,7 @@ class MakeAccount extends Component {
                             <Input type="checkbox" checked={this.state.special} onChange={this.changeSpecial} />특수계좌
                         </Label>
                         <Col>
-                            <Input type="select" name="grade" id="grade" sm={8} style={{ color: 'grey' }} disabled={!this.state.special}>
+                            <Input type="select" name="grade" id="grade" sm={8} style={{ color: 'grey' }} disabled={!this.state.special} onChange={this.channge}>
                                 <option>VIP</option>
                                 <option>Gold</option>
                                 <option>Silver</option>
