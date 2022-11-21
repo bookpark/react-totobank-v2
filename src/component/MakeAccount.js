3dc35592c3cf3c28f1f98a77aba9582d0c9f6b96
bookpark <bkbkgg@gmail.com>
2022-11-21 10:56:14 +0900

    checkbox를 위한 onChange 함수 추가

--- src/component/MakeAccount.js
+++ src/component/MakeAccount.js
@@ -6,9 +6,29 @@ class MakeAccount extends Component {
         super(props);
         this.divStyle = {
             width: '600px', height: '430px', textAlign: 'left',
-            margin: '80px auto', border: '2px solid grey',
+            margin: '100px auto', border: '2px solid grey',
             padding: '40px', borderRadius: '20px'
         };
+        this.state = {
+            acc: {
+                id: '',
+                name: '',
+                password: '',
+                grade: ''
+            },
+            special: false,
+            modal: false,
+            msg_header: '',
+            msg_body: ''
+        }
+    }
+
+    changeSpecial = (e) => {
+        this.setState({ special: e.target.checked })
+    }
+
+    toggle = () => {
+        this.setState({ modal: !this.state.modal });
     }
 
     render() {
@@ -17,8 +37,8 @@ class MakeAccount extends Component {
                 <Form>
                     <FormGroup row>
                         <Label for="id" sm={4}>계 좌&nbsp; 번 호</Label>
-                        <Col>
-                            <Input type="text" name="id" id="id" sm={6} />
+                        <Col sm={5}>
+                            <Input type="text" name="id" id="id" sm={6} value={this.state.acc.id} />
                         </Col>
                         <Col>
                             <Button sm={2} color='primary' style={{ width: '100px' }}>중복</Button>
@@ -27,22 +47,22 @@ class MakeAccount extends Component {
                     <FormGroup row>
                         <Label for="name" sm={4}>이&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;름</Label>
                         <Col>
-                            <Input type="text" name="name" id="name" sm={8} />
+                            <Input type="text" name="name" id="name" sm={8} value={this.state.acc.name} />
                         </Col>
                     </FormGroup>
                     <FormGroup row>
                         <Label for="password" sm={4}>비 밀&nbsp; 번 호</Label>
                         <Col>
-                            <Input type="password" name="password" id="password" sm={8} />
+                            <Input type="password" name="password" id="password" sm={8} value={this.state.acc.password} />
                         </Col>
                     </FormGroup>
                     <FormGroup row>
                         <Label check sm={4}>
                             {/* {' '} << 한칸 띄우고 싶을 때 사용 가능 */}
-                            <Input type="checkbox" />특수계좌
+                            <Input type="checkbox" checked={this.state.special} onChange={this.changeSpecial} />특수계좌
                         </Label>
                         <Col>
-                            <Input type="select" name="grade" id="grade" sm={8} style={{ color: 'grey' }}>
+                            <Input type="select" name="grade" id="grade" sm={8} style={{ color: 'grey' }} disabled={!this.state.special}>
                                 <option>VIP</option>
                                 <option>Gold</option>
                                 <option>Silver</option>
@@ -56,6 +76,17 @@ class MakeAccount extends Component {
                         </Col>
                     </FormGroup>
                 </Form>
+
+                <Modal isOpen={this.state.modal} fade={true} toggle={this.toggle}>
+                    <ModalHeader toggle={this.toggle}>{this.state.msg_header}</ModalHeader>
+                    <ModalBody>
+                        {this.state.msg_body}
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="secondary" onClick={this.toggle}>닫기</Button>
+                    </ModalFooter>
+                </Modal>
+
             </div>
         )
     }
