ac11804310a3271b26e5a756204c7fec55f3c55b
bookpark <bkbkgg@gmail.com>
2022-11-22 14:56:03 +0900

    출금 기능 추가

--- src/component/App.js
+++ src/component/App.js
@@ -1,22 +1,23 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import { Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from './Header';
 import Main from './Main';
 import MakeAccount from './MakeAccount';
 import InquireAccount from './InquireAccount';
 import Deposit from './Deposit';
+import Withdraw from './Withdraw';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <Routes>
-        <Route exact path='/' element={<Main />}/>
+        <Route exact path='/' element={<Main />} />
         <Route exact path='/make-account' element={<MakeAccount />} />
         <Route exact path='/account-info' element={<InquireAccount />} />
         <Route exact path='/deposit' element={<Deposit />} />
-        {/* <Route exact path='/withdraw' element={<Withdraw />} /> */}
+        <Route exact path='/withdraw' element={<Withdraw />} />
       </Routes>
     </div>
   );
